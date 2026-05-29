94c7f9fc3f862ebd9f5cbeed345694e66c701774
Lisandro <lisandrofernandezet32@gmail.com>
2026-05-29 14:53:07 -0300

    Update PRUEBALISANDRO.ino

--- PRUEBALISANDRO.ino
+++ PRUEBALISANDRO.ino
@@ -1,36 +1,41 @@
 #define valorboton 2
 #define luz A0
 #define movi 5
+#define rojo 3
+#define azul 6
+// VARIABLES:
 bool boton;
 bool movimiento;
 int valorluz;
 int realvalorluz;
 void setup()
 {
-  pinMode(2, INPUT);
-   pinMode(3 ,OUTPUT);
-   pinMode(6 ,OUTPUT);
-  pinMode(5,INPUT);
+  pinMode(valorboton, INPUT);
+   pinMode(rojo ,OUTPUT);
+   pinMode(azul,OUTPUT);
+  pinMode(movi,INPUT);
   Serial.begin(9800);
 }
 
 void loop()
 {
+// DEFINO VALOR DE VARIABLES
 boton = digitalRead(valorboton);
 valorluz = analogRead(luz);
-  movimiento = digitalRead(movi);
+movimiento = digitalRead(movi);
+// IMPRIMO EL VALOR DE LA LUZ PARA QUE EL USUARIO SEPA CUANTO VALE
 Serial.println("El valor de la luz es de: ");
-  Serial.println(realvalorluz);
-  Serial.println(movimiento);
- realvalorluz = map(valorluz,22,671,0,100);
-if (boton == LOW && realvalorluz > 50){
+Serial.println(realvalorluz);
+realvalorluz = map(valorluz,22,671,0,100);
+// IF QUE HACE FUNCIONAR A TODO
+  if (boton == LOW && realvalorluz > 50){
   if (movimiento == HIGH){
-    analogWrite(3,255);
-    analogWrite(6,255);
+    analogWrite(rojo,255);
+    analogWrite(azul,255);
   }
   else if (movimiento == LOW){
-    analogWrite(3,0);
-    analogWrite(6,0);
+    analogWrite(rojo,0);
+    analogWrite(azul,0);
   }
 }
 }
