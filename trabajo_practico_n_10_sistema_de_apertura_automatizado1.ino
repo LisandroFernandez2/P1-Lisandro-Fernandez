575597addc2750bd7269bfddec78988037493938
Lisandro <lisandrofernandezet32@gmail.com>
2026-08-23 19:26:50 -0300

    Add files via upload

--- trabajo_practico_n_10_sistema_de_apertura_automatizado1.ino
+++ trabajo_practico_n_10_sistema_de_apertura_automatizado1.ino
@@ -1,4 +1,5 @@
 #include <Servo.h>
+#include <Adafruit_LiquidCrystal.h>
 #define ValorDelServo1 A0
 #define ValorDelServo2 A1
 #define ValorDelSensorM1 2
@@ -11,16 +12,13 @@
 #define F4 349
 #define A4 440
 #define C4 262
-
-// VARIABLES
+Adafruit_LiquidCrystal lcd1(0);
 Servo Servo1;
 Servo Servo2;
-bool ValorSensorMovimiento1;
-bool ValorSensorMovimiento2;
+bool ValorMovimiento1;
+bool ValorMovimiento2;
+bool EstaAbierta = LOW;
 int ValorFotoresistencia;
-
-
-//==========================================================================================================================
 
 const int midi1[5][3] = {
  {B3, 136, 136},
@@ -37,7 +35,6 @@ const int midi2[5][3] = {
  {C4, 136, 0},
 };
 
-
 void playMidi(int pin, const int notes[][3], size_t len){
  for (int i = 0; i < len; i++) {
     tone(pin, notes[i][0]);
@@ -47,67 +44,89 @@ void playMidi(int pin, const int notes[][3], size_t len){
   }
 }
 
-void SeAbrenLasPuertas(bool Movimiento){
+void VerificarMovimiento()
+{
+   ValorMovimiento1 = digitalRead(ValorDelSensorM1);
+ ValorMovimiento2 = digitalRead(ValorDelSensorM2);
+  
+  if (ValorMovimiento1 == HIGH && EstaAbierta == LOW)
+  {
+   abrir();
+   EstaAbierta = HIGH;
+  }
+  else if (ValorMovimiento1 == HIGH && EstaAbierta == HIGH)
+  {
+   cerrar();
+   EstaAbierta = LOW;
+  }
+  delay(2000);
+  if (ValorMovimiento2 == HIGH && EstaAbierta == LOW)
+  {
+   abrir();
+   EstaAbierta = HIGH;
+  }
+  else if (ValorMovimiento2 == HIGH && EstaAbierta == HIGH)
+  {
+   cerrar(); 
+   EstaAbierta = LOW;
+  }
+}
+
+void HayLuz(){
+ ValorFotoresistencia = analogRead(A2); 
+  if (ValorFotoresistencia > 500){
+    digitalWrite(ValorLuz, HIGH);
+  }
+  else{
+    digitalWrite(ValorLuz, LOW);
+  }  
+}
+
+void abrir(){
   playMidi(3, midi1, ARRAY_LEN(midi1));
   Servo1.write(180);
   Servo2.write(180);
-  
+  delay(1000);
 }
-void ServosCerrados(){
-  HayLuz();
+
+void cerrar(){
+  playMidi(3, midi2, ARRAY_LEN(midi2));
   Servo1.write(90);
   Servo2.write(90);
+  delay(1000);
 }
-void VerificarMovimiento(){
-  if (ValorSensorMovimiento1 == HIGH){
-    SeAbrenLasPuertas(ValorSensorMovimiento1);
-  	}
-  if (ValorSensorMovimiento2 == HIGH){
-    playMidi(3, midi2, ARRAY_LEN(midi1));
-    ServosCerrados();
-	}
-  if (ValorSensorMovimiento1 == LOW && ValorSensorMovimiento2 == LOW){
-    ServosCerrados();
-  	}
-}
-void HayLuz(){
-  if (ValorFotoresistencia > 500){
-    digitalWrite(ValorLuz,HIGH);
-  }
-  else{
-    digitalWrite(ValorLuz,LOW);
-  }  
-}
+
 void setup()
 {
-// VALORES DE LOS SERVOS 
- Servo1.attach(ValorDelServo1);
- Servo2.attach(ValorDelServo2);
- Servo1.write(0);
- Servo2.write(0);
-//======================================================================================  
-// VALORES DE LOS PINES (INPUT/OUTPUT)  
-  pinMode(ValorDelServo1,INPUT);
-  pinMode(ValorDelServo2,INPUT);
-  pinMode(ValorDelSensorM1,INPUT);
-  pinMode(ValorDelSensorM2,INPUT);
-  pinMode(ValorDelBozzer,INPUT);
-  pinMode(ValorLuz,OUTPUT);
-  pinMode(A2,INPUT);	
-//======================================================================================  
+  Servo1.attach(ValorDelServo1);
+  Servo2.attach(ValorDelServo2);
+  Servo1.write(90);
+  Servo2.write(90);
+  lcd1.begin(16,2);
+  
+  pinMode(ValorDelSensorM1, INPUT);
+  pinMode(ValorDelSensorM2, INPUT);
+  pinMode(A2, INPUT);	
+  
+  pinMode(ValorDelBozzer, OUTPUT);
+  pinMode(ValorLuz, OUTPUT);
+  
+  Serial.begin(9800);
 }
 
-
-
-
-
-
+void LcdAbiertoOCerrado(){
+     if (EstaAbierta == LOW){
+    lcd1.setCursor(0,0);
+     lcd1.print("Cerrado");
+  }
+    else{
+      lcd1.setCursor(0,0);
+      lcd1.print("Abierto");
+    }
+}
 void loop()
-{
-ValorSensorMovimiento1 = digitalRead(ValorDelSensorM1);
-ValorSensorMovimiento2 = digitalRead(ValorDelSensorM2);
-ValorFotoresistencia = analogRead(A2);  
-// FUNCION QUE VERIFICA SI HAY MOVIMIENTO  
- VerificarMovimiento();
- HayLuz();
+{  
+  VerificarMovimiento();
+  HayLuz();
+  LcdAbiertoOCerrado();
 }
